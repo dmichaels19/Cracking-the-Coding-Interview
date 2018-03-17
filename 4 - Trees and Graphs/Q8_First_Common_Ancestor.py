89b41dfdf3c831f831da81b9443eccefea2febc6
David W Grossman <2gotgrossman@gmail.com>
2018-03-17 13:20:55 -0400

    added comment and removed unecessary if

--- 4 - Trees and Graphs/Q8_First_Common_Ancestor.py
+++ 4 - Trees and Graphs/Q8_First_Common_Ancestor.py
@@ -17,8 +17,9 @@ def find_common_ancestor(curr_node, n1, n2):
     :param curr_node:
     :param n1:
     :param n2:
-    :return: 3-tuple: whether subtree has n1, whether subtree has n2, whether subtree has common ancestor. If so,
-                    return the value
+    :return: 3-tuple: whether subtree has n1 (Boolean), whether subtree has n2 (Boolean), 
+                whether subtree has common ancestor (Boolean or the node itself if True). If so,
+                return the value.
     """
     if curr_node is None:
         return False, False, False
@@ -26,8 +27,8 @@ def find_common_ancestor(curr_node, n1, n2):
     left_has_n1, left_has_n2, left_has_common_node = find_common_ancestor(curr_node.left, n1, n2)
     right_has_n1, right_has_n2, right_has_common_node = find_common_ancestor(curr_node.left, n1, n2)
 
-    curr_has_n1 = True if curr_node.data == n1 else False
-    curr_has_n2 = True if curr_node.data == n2 else False
+    curr_has_n1 = curr_node.data == n1 
+    curr_has_n2 = curr_node.data == n2 
 
     # Is true if left subtree, right subtree, or current node has the desired N
     return_n1 = left_has_n1 or right_has_n1 or curr_has_n1
