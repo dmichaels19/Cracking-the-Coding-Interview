989f3987344ee6ea580be4cb4a935f8694808ed7
David W Grossman <2gotgrossman@gmail.com>
2018-03-17 13:11:59 -0400

    Spacing

--- 4 - Trees and Graphs/Q9_BST_Sequences.py
+++ 4 - Trees and Graphs/Q9_BST_Sequences.py
@@ -1,5 +1,6 @@
 """
-A binary search tree was created by traversing throug han array from left to right and inserting each element. give a binary search tree with distinct elements, print all possible arrays that could have led to this tree.
+A binary search tree was created by traversing throug han array from left to right and inserting each element. 
+Given a binary search tree with distinct elements, print all possible arrays that could have led to this tree.
 """
 
 from collections import deque
