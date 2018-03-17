1aa2354d47c68af969c3989bfbb2c9c248928b59
David W Grossman <2gotgrossman@gmail.com>
2018-03-17 13:24:31 -0400

    Cleaned up format

--- 4 - Trees and Graphs/Q3_List_Of_Depths.py
+++ 4 - Trees and Graphs/Q3_List_Of_Depths.py
@@ -1,21 +1,21 @@
+"""
+Chapter 4, Question 3
+Given a binary tree, design an algorithm which creates a list (array or linked list) of
+all the nodes at each depth (e.g., if you have a tree with depth D, you’ll have D lists).
+"""
+
 class BinaryTreeNode():
     def __init__(self, value, left, right):
         self.value = value
         self.left = left
         self.right = right
 
-def listOfDepths(tree):
-    """
-
-    :param tree:
-    :return:
-    """
-
+def list_of_depths(tree):
     DepthsList = []
 
-    recurse(tree, 0, DepthsList)
+    find_depths(tree, 0, DepthsList)
 
-def recurse(tree, height, DepthsList):
+def find_depths(tree, height, DepthsList):
     if tree is None:
         return
 
